77d2706e2611b171b4a8e0de7d6b9115b9b6dcd3
Okamoto2023 <2212110239c@kindai.ac.jp>
2023-06-14 08:03:34 +0900

    天気の詳細な情報の取得をやめた

--- mqtt_server.py
+++ mqtt_server.py
@@ -1,5 +1,4 @@
 import paho.mqtt.client as mqtt
-
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -15,19 +14,11 @@ def main(url):
     l_pattern = r"(.+)の今日明日の天気"
     l_src = s.title.text
     dict['location'] = re.findall(l_pattern, l_src)[0]
-    # print(dict['location'] + "の天気")
-
+    print(dict['location'] + "の天気")
     soup_tdy = s.select('.today-weather')[0]
-    soup_tmr = s.select('.tomorrow-weather')[0]
 
     dict["today"] = forecast2dict(soup_tdy)
-    # print("雨" in dict["today"]["forecasts"][0]["weather"])
     return dict["today"]
-    # dict["tomorrow"] = forecast2dict(soup_tmr)
-
-    # #JSON形式で出力
-    # print(json.dumps(dict, ensure_ascii=False)["forecasts"])
-    # data = json.loads(json_data)
 
 def soup(url):
     r = requests.get(url)
@@ -42,34 +33,14 @@ def forecast2dict(soup):
     d_src = soup.select('.left-style')
     date = re.findall(d_pattern, d_src[0].text)[0]
     data["date"] = "%s-%s(%s)" % (date[0], date[1], date[2])
-    # print("=====" + data["date"] + "=====")
+    print("=====" + data["date"] + "=====")
 
     # ## 取得
     weather           = soup.select('.weather-telop')[0]
-    high_temp         = soup.select("[class='high-temp temp']")[0]
-    high_temp_diff    = soup.select("[class='high-temp tempdiff']")[0]
-    low_temp          = soup.select("[class='low-temp temp']")[0]
-    low_temp_diff     = soup.select("[class='low-temp tempdiff']")[0]
-    rain_probability  = soup.select('.rain-probability > td')
-    wind_wave         = soup.select('.wind-wave > td')[0]
-
     # ## 格納
     data["forecasts"] = []
     forecast = {}
     forecast["weather"] = weather.text.strip()
-    forecast["high_temp"] = high_temp.text.strip()
-    forecast["high_temp_diff"] = high_temp_diff.text.strip()
-    forecast["low_temp"] = low_temp.text.strip()
-    forecast["low_temp_diff"] = low_temp_diff.text.strip()
-    every_6h = {}
-    for i in range(4):
-        time_from = 0+6*i
-        time_to   = 6+6*i
-        itr       = '{:02}-{:02}'.format(time_from,time_to)
-        every_6h[itr] = rain_probability[i].text.strip()
-    forecast["rain_probability"] = every_6h
-    forecast["wind_wave"] = wind_wave.text.strip()
-
     data["forecasts"].append(forecast)
 
     print("天気              ： " + forecast["weather"])
@@ -112,5 +83,3 @@ client.connect(broker_address, broker_port)
 
 # メッセージの待機と処理
 client.loop_forever()
-
-###########################################################
